f82be944762a2fd0e3dcb22b16698a6907f34fe0
iCode <gbengaobafemi1@gmail.com>
2021-06-16 13:54:38 +0100

    CEH

--- controllers/contact.js
+++ controllers/contact.js
@@ -35,18 +35,17 @@ exports.createContact = asyncHandler(async (req, res, next) => {
 exports.cehRegistration = asyncHandler(async (req, res, next) => {
   const message = `Dear Team, ${req.body.firstname}  ${req.body.lastname}just registered for CEH, \n\n Firstname: ${req.body.firstname} \n\n Lastname: ${req.body.lastname}
   \n\n Email: ${req.body.email}\n\n Mobile: ${req.body.mobile}\n\n Country: ${req.body.country}\n\n Intrest: ${req.body.intrest} \n\n Training: ${req.body.training}`;
-
+  const ceh = await Ceh.create(req.body);
+  res.status(200).json({
+    success: true,
+    data: ceh,
+  });
   try {
     await sendEmail({
       email: "iyaki@lotusbetaanalytics.com",
       subject: "CEH",
       cc: "ayomide@lotusbetaanalytics.com",
       message,
-    });
-    const ceh = await Ceh.create(req.body);
-    res.status(200).json({
-      success: true,
-      data: ceh,
     });
   } catch (err) {
     console.log(err);
